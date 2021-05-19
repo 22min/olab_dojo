0264a06f5946b187aed8d5f132fc99349d2d54f4
Masala <masalachai@redbook>
2021-05-19 22:04:28 +0900

    レヴェル2

---
+++ lv2/change_message.js
@@ -0,0 +1,9 @@
+window.onload= function(){
+    const header = document.querySelector('h1');
+    header.innerHTML = 'Hello, HTML!';
+}
+
+function change_message(){
+    const header = document.querySelector('h1');
+    header.innerHTML = 'Hello, JS!';
+}
