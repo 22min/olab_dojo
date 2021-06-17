bb9e56efad42471ad5c706acdedcea6733cdbaf5
Masala <masalachai@redbook>
2021-06-18 02:32:36 +0900

    デフォルト値を「いろは」に。空白文字、改行文字を除く文字数も併記するように。

--- lv7/char_counter/pages/index.vue
+++ lv7/char_counter/pages/index.vue
@@ -1,20 +1,31 @@
 <template>
   <div>
     <h1>文字数カウンター</h1>
-    <textarea style="width:300px; height:150px;" v-model="text"></textarea>
-    <p>文字数: <span>{{ text.length }}</span></p>
+    <textarea style="width:300px; height:150px;" v-model="text" ></textarea>
+    <p>文字数: <span>{{ charNum }}</span><br>
+    文字数(空白文字、改行文字を除く): <span>{{charNumWithoutwhitespaceAndNewline}}</span>
+    </p>
+
   </div>
 </template>
 
 <script>
 export default {
-name: "CharCounter",
-
-
+  name: "CharCounter",
   data: () => ({
-    text: "", 
-
+    text: `いろはにほへと　ちりぬるを
+わかよたれそ　　つねならむ
+うゐのおくやま　けふこえて
+あさきゆめみし　ゑひもせすん`, 
   }),
+  computed:{
+    charNum: function(){
+      return this.text.length;
+    },
+    charNumWithoutwhitespaceAndNewline(){
+      return this.text.replace(/[(\r?\n)\s]/g,"").length;
+    }
+  }
 }
 </script>
 
