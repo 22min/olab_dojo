b637e2c355d2bb0a3eb1cc87cf2ade3488d59825
Masala <masalachai@redbook>
2021-06-18 01:07:41 +0900

    lv7を追加

---
+++ lv7/char_counter/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>文字数カウンター</h1>
+    <textarea style="width:300px; height:150px;" v-model="text"></textarea>
+    <p>文字数: <span>{{ text.length }}</span></p>
+  </div>
+</template>
+
+<script>
+export default {
+name: "CharCounter",
+
+
+  data: () => ({
+    text: "", 
+
+  }),
+}
+</script>
+
+<style>
+</style>
